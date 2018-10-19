37f676123b290ea1f0b7da1e469904274d58a3eb
Nicolas Gonzalez Paz <nico_tate07@hotmail.com>
2018-10-18 22:28:28 -0300

    Terminada ventana historial

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/HistorialPedidoActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/HistorialPedidoActivity.java
@@ -1,10 +1,14 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -13,6 +17,7 @@ import java.util.List;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.PedidoRepository;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.ProductoRepository;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.PedidoAdapter;
 
 
 public class HistorialPedidoActivity extends AppCompatActivity {
@@ -28,6 +33,8 @@ public class HistorialPedidoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.historial_pedido);
+        final Intent intentNuevoPedido = new Intent(this, NuevoPedidoActivity.class);
+        final Intent intentMenuPrincipal = new Intent(this, MainActivity.class);
 
         btnHistorialMenu = findViewById(R.id.btnHistorialMenu);
         btnHistorialNuevo = findViewById(R.id.btnHistorialNuevo);
@@ -38,9 +45,32 @@ public class HistorialPedidoActivity extends AppCompatActivity {
 
         final List<Pedido> lstPedido = pedidoRepository.getLista();
 
-        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,android.R.id.text1,pedido.getDetalle());
-        lstHistorialPedidos.setAdapter(adapter);
+        //adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,android.R.id.text1,lstPedido);
+        adapter = new PedidoAdapter(this,lstPedido);
+        lstHistorialPedidos.setAdapter((ListAdapter) adapter);
 
+        btnHistorialNuevo.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(intentNuevoPedido);
+            }
+        });
 
+        btnHistorialMenu.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(intentMenuPrincipal);
+            }
+        });
+
+        lstHistorialPedidos.setOnItemLongClickListener(new ListView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Pedido p = (Pedido) adapterView.getItemAtPosition(i);
+                intentNuevoPedido.putExtra("Id pedido", p.getId());
+                startActivity(intentNuevoPedido);
+                return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/modelo/PedidoAdapter.java
@@ -0,0 +1,112 @@
+package ar.edu.utn.frsf.dam.isi.laboratorio02.modelo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.DataSetObserver;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ar.edu.utn.frsf.dam.isi.laboratorio02.NuevoPedidoActivity;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.R;
+
+public class PedidoAdapter extends ArrayAdapter<Pedido> {
+    private Context ctx;
+    private List<Pedido> datos;
+
+    public PedidoAdapter(@NonNull Context context, @NonNull List<Pedido> objects) {
+        super(context, 0, objects);
+        this.ctx = context;
+        this.datos = objects;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        LayoutInflater inflater = LayoutInflater.from(this.getContext());
+        View fila_historial = convertView;
+        if(fila_historial == null){
+            fila_historial = inflater.inflate(R.layout.fila_historial, parent, false);
+        }
+
+        PedidoHolder pediHolder = (PedidoHolder) fila_historial.getTag();
+        if(pediHolder == null)
+        {
+            pediHolder = new PedidoHolder(fila_historial);
+            fila_historial.setTag(pediHolder);
+        }
+
+
+        Pedido pedido = (Pedido) super.getItem(position);
+        pediHolder.tvMailPedido.setText("Contacto: "+pedido.getMailContacto());
+        pediHolder.tvHoraEntrega.setText("Fecha de Entrega: "+pedido.getFecha().toString());
+        pediHolder.tvCantidadItems.setText("Items: "+new Integer(pedido.getDetalle().size()).toString());
+        pediHolder.tvPrecio.setText("A pagar $: "+pedido.total().toString());
+
+        //Ponemos esta imagen por que no encontramos la pedida
+        pediHolder.tipoEntrega.setImageResource(R.drawable.utencillos);
+
+        switch(pedido.getEstado())
+        {
+            case LISTO:
+                pediHolder.tvEstado.setTextColor(Color.DKGRAY);
+                break;
+            case ACEPTADO:
+                pediHolder.tvEstado.setTextColor(Color.GREEN);
+                break;
+            case ENTREGADO:
+                pediHolder.tvEstado.setTextColor(Color.BLUE);
+                break;
+            case REALIZADO:
+                pediHolder.tvEstado.setTextColor(Color.BLUE);
+                break;
+            case CANCELADO:
+            case RECHAZADO:
+                pediHolder.tvEstado.setTextColor(Color.RED);
+                break;
+            case EN_PREPARACION:
+                pediHolder.tvEstado.setTextColor(Color.MAGENTA);
+                break;
+            default:
+                throw new RuntimeException("UNREACHABLE");
+        }
+        pediHolder.tvEstado.setText(pedido.getEstado().toString());
+        pediHolder.btnCancelar.setTag(pedido);
+        pediHolder.btnDetalles.setTag(pedido);
+
+        pediHolder.btnCancelar.setOnClickListener(
+            new Button.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Pedido p = (Pedido) view.getTag();
+                    if(     p.getEstado().equals(Pedido.Estado.REALIZADO)||
+                            p.getEstado().equals(Pedido.Estado.ACEPTADO) ||
+                            p.getEstado().equals(Pedido.Estado.EN_PREPARACION))
+                    {
+                        p.setEstado(Pedido.Estado.CANCELADO);
+                        PedidoAdapter.this.notifyDataSetChanged();
+                        return;
+                    }
+                }
+            }
+        );
+        final Intent intentNuevoPedido = new Intent(this.ctx, NuevoPedidoActivity.class);
+        pediHolder.btnDetalles.setOnClickListener(
+        new Button.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  intentNuevoPedido.putExtra("Id pedido", ((Pedido)view.getTag()).getId());
+                  ctx.startActivity(intentNuevoPedido);
+              }
+          }
+        );
+        return fila_historial;
+    }
+}
